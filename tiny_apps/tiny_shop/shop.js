3964ebf106a4d606124035a6be71404055fc705f
Mark Vincent <mvrktopher@gmail.com>
2021-10-21 13:52:52 -0400

    Add Tiny Shop

---
+++ tiny_apps/tiny_shop/shop.js
@@ -0,0 +1,26 @@
+// Layout === All other apps.
+// DO NOT AMMEND INDEX.HTML BODY.
+// Write all code in this file. 
+
+// DISPLAY
+// User lands on page => grid of items
+// grid item has an image, price, "buy button"
+// buy button => adds item to cart
+
+// OPTIONS
+// Cart Button innerHTML = Cart: $0.00
+// Cart Button updates total value of the cart
+// Clicking cart button => REPLACE DISPLAY w/ Items in the cart
+// Check Out => Clears Cart
+// Cart Button => "Back to Shop"
+
+// Out of Scope
+// "Backend" persists and serves Shop Items
+// NOT process cart, sign up users, etc. 
+
+
+// STACK
+// HTML/CSS/JS => Test w/ Local Storage
+// Production => DB (NoSQL), Backend (Severless), Image Storage (AWS S3);
+
+// Research: 
